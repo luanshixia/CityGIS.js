bced905e8de3b1573df0eec481139e9bd14d1437
luanshixia <luanshixia@gmail.com>
2014-08-28 14:18:01 +0800

    gis.ts
    add gis.ts

--- src/gis.js
+++ src/gis.js
@@ -1,10 +1,77 @@
-/**
-* gis.js
-*/
+/// <reference path="geometry.ts" />
 var dreambuild;
 (function (dreambuild) {
+    /**
+    * gis.js
+    */
     (function (gis) {
         "use strict";
+
+        var Map = (function () {
+            function Map(name) {
+                this.name = name;
+                this.layers = [];
+            }
+            Map.prototype.extents = function () {
+                return this.layers.map(function (l) {
+                    return l.extents();
+                }).reduce(function (e1, e2) {
+                    return e1.add(e2);
+                });
+            };
+
+            Map.prototype.serialize = function () {
+                return JSON.stringify(this);
+            };
+
+            Map.deserialize = function (json) {
+                return JSON.parse(json);
+            };
+            return Map;
+        })();
+        gis.Map = Map;
+
+        var Layer = (function () {
+            function Layer(name, type) {
+                this.name = name;
+                this.type = type;
+                this.features = [];
+            }
+            Layer.prototype.extents = function () {
+                return this.features.map(function (f) {
+                    return f.extents();
+                }).reduce(function (e1, e2) {
+                    return e1.add(e2);
+                });
+            };
+            return Layer;
+        })();
+        gis.Layer = Layer;
+
+        var Feature = (function () {
+            function Feature(id, geometry) {
+                this.id = id;
+                this.geometry = geometry;
+                this.properties = {};
+            }
+            Feature.prototype.extents = function () {
+                return new dreambuild.geometry.PointString(this.geometry).extents();
+            };
+            return Feature;
+        })();
+        gis.Feature = Feature;
+
+        (function (GeometricType) {
+            GeometricType[GeometricType["unknown"] = 0] = "unknown";
+            GeometricType[GeometricType["point"] = 1] = "point";
+            GeometricType[GeometricType["linear"] = 2] = "linear";
+            GeometricType[GeometricType["region"] = 4] = "region";
+            GeometricType[GeometricType["solid"] = 8] = "solid";
+            GeometricType[GeometricType["multiPoints"] = 17] = "multiPoints";
+            GeometricType[GeometricType["multiLinears"] = 18] = "multiLinears";
+            GeometricType[GeometricType["multiRegions"] = 20] = "multiRegions";
+        })(gis.GeometricType || (gis.GeometricType = {}));
+        var GeometricType = gis.GeometricType;
     })(dreambuild.gis || (dreambuild.gis = {}));
     var gis = dreambuild.gis;
 })(dreambuild || (dreambuild = {}));
--- src/gis.ts
+++ src/gis.ts
@@ -1,3 +1,4 @@
+/// <reference path="geometry.ts" />
 
 /**
  * gis.js
@@ -5,4 +6,72 @@
 
 module dreambuild.gis {
     "use strict";
+
+    export class Map {
+
+        name: string;
+        layers: Layer[];
+
+        constructor(name?: string) {
+            this.name = name;
+            this.layers = [];
+        }
+
+        extents() {
+            return this.layers.map(l => l.extents()).reduce((e1, e2) => e1.add(e2));
+        }
+
+        serialize() {
+            return JSON.stringify(this);
+        }
+
+        static deserialize(json: string) {
+            return <Map>JSON.parse(json);
+        }
+    }
+
+    export class Layer {
+
+        name: string;
+        type: GeometricType;
+        features: Feature[];
+
+        constructor(name?: string, type?: GeometricType) {
+            this.name = name;
+            this.type = type;
+            this.features = [];
+        }
+
+        extents() {
+            return this.features.map(f => f.extents()).reduce((e1, e2) => e1.add(e2));
+        }
+    }
+
+    export class Feature {
+
+        id: string;
+        geometry: geometry.Vector[];
+        properties: { [name: string]: string };
+
+        constructor(id?: string, geometry?: geometry.Vector[]) {
+            this.id = id;
+            this.geometry = geometry;
+            this.properties = {};
+        }
+
+        extents() {
+            return new geometry.PointString(this.geometry).extents();
+        }
+    }
+
+    export enum GeometricType {
+        unknown = 0,
+        point = 1,
+        linear = 2,
+        region = 4,
+        solid = 8,
+        multiPoints = 17,
+        multiLinears = 18,
+        multiRegions = 20
+    }
 }
